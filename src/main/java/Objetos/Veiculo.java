e4fbe51b18e955f24bccd94c8b204872f86c70fe
Fillipe <Fillipe@192.168.15.7>
2021-05-25 15:42:12 -0300

    Realizada a atividade proposta em aula

---
+++ src/main/java/Objetos/Veiculo.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objetos;
+
+/**
+ *
+ * @author Fillipe
+ */
+public class Veiculo {
+    private String fabricante;
+    private String modelo;
+    private Double preco;
+
+    /**
+     * @return the fabricante
+     */
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    /**
+     * @param fabricante the fabricante to set
+     */
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the preco
+     */
+    public Double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    
+    
+}
